a84a21d41e826903774334aa9935681b8287f9ad
Camilo Oviedo <camilo.oviedof@gmail.com>
2021-11-24 21:18:16 -0300

    Conexion a bd mediante mysql

--- src/main/java/cl/bayer/customer/model/entity/EspecieSemilla.java
+++ src/main/java/cl/bayer/customer/model/entity/EspecieSemilla.java
@@ -3,11 +3,15 @@ package cl.bayer.customer.model.entity;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+@Entity
+@Table(name="especie_semilla")
 public class EspecieSemilla implements Serializable{
 
 	/**
