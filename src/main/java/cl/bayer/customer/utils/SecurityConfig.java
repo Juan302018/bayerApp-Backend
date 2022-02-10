a2ce4379db5daa289009772f442937e9457f02cd
Juan302018 <jfernandez816@hotmail.com>
2022-02-10 13:25:34 -0300

    Configuración HTTPS y creación del certificado SSL

---
+++ src/main/java/cl/bayer/customer/utils/SecurityConfig.java
@@ -0,0 +1,21 @@
+package cl.bayer.customer.utils;
+
+import org.springframework.core.Ordered;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE + 10)
+public class SecurityConfig {
+	  @Bean
+	  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    return http
+	      .requiresChannel(channel -> 
+	          channel.anyRequest().requiresSecure())
+	      .authorizeRequests(authorize ->
+	          authorize.anyRequest().permitAll())
+	      .build();
+	    }
+}
